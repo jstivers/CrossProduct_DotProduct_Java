bcb58d21c169f04142c33fc2ec149e55f404f3d5
John Stivers <jstivers@usc.edu>
2017-01-27 22:12:08 -0800

    fixed! was accidentally using == instead of .equals() while comparing strings

--- src/CrossProduct_DotProduct.java
+++ src/CrossProduct_DotProduct.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CrossProduct_DotProduct{
@@ -7,28 +8,30 @@ public class CrossProduct_DotProduct{
     }
     public  static void DecisionFunction(){
         System.out.println("Welcome to the Cross Product and Dot Product Calculator!");
-        System.out.println("If you would like to calculate a cross product, enter /'Cross Product/', if you would like " +
-                "to enter a dot product enter /'Dot Product/' or /'Exit/' to end the program.");
+        System.out.println("If you would like to calculate a cross product, enter \"Cross Product\", if you would like " +
+                "to enter a dot product enter \"Dot Product\" or \"Exit\" to end the program.");
         Scanner keyboard = new Scanner(System.in);
         String choice = keyboard.nextLine();
+
         Double[] VectorOne;
         Double[] VectorTwo;
-        if (choice == "Quit") {
+
+        if (choice.equals("Quit")){
             System.exit(0);
         }
-        else if (choice=="Cross Product"){
+        else if (choice.equals("Cross Product")){
             System.out.println("Please enter the values of Vector one, separated by commas");
             String container1 = keyboard.nextLine();
             System.out.println("Please enter the values of Vector two, separated by commas");
             String container2 = keyboard.nextLine();
-            CrossProduct(container1,container2);
+            Double[] CrossProductResult = (CrossProduct(container1,container2));
             System.out.println(CrossProductResult);
         }
-        else if (choice == "Dot Product"){
+        else if (choice.equals("Dot Product")){
             System.exit(0);
         }
     }
-    public static void CrossProduct(String container1 ,String container2){
+    public static Double[] CrossProduct(String container1 , String container2){
         Double[] CrossProductResult = {0.0,0.0,0.0};
         String[] SplitStringOne = container1.split(",");
         Double[] VectorArrayDoubleOne = new Double[10];
@@ -44,7 +47,7 @@ public class CrossProduct_DotProduct{
         CrossProductResult[1] = (VectorArrayDoubleOne[0]*VectorArrayDoubleTwo[2]-VectorArrayDoubleOne[2]*VectorArrayDoubleTwo[0]);
         CrossProductResult[2] = (VectorArrayDoubleOne[0]*VectorArrayDoubleTwo[1]-VectorArrayDoubleOne[0]*VectorArrayDoubleTwo[1]);
 
-
+        return CrossProductResult;
 
 
 
